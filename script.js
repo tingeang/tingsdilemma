361948eea4b38317d10d66649f52366fdf8bb671
tingeang <kloetaph@risd.edu>
2016-11-14 21:34:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,326 @@
+window.addEventListener('load', function(){
+
+
+var image1 = document.getElementById ("rotate1");
+image1.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image1.addEventListener("mouseout", function () {
+	var image1 = this;
+	image1.className = "out";
+	window.setTimeout (function () { image1.className = "" }, 90 );
+}, false );
+
+var image2 = document.getElementById ("rotate2");
+image2.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image2.addEventListener("mouseout", function () {
+	var image2 = this;
+	image2.className = "out1";
+	window.setTimeout (function () { image2.className = "" }, 90 );
+}, false );
+
+var image3 = document.getElementById ("rotate3");
+image3.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image3.addEventListener("mouseout", function () {
+	var image3 = this;
+	image3.className = "out2";
+	window.setTimeout (function () { image3.className = "" }, 90 );
+}, false );
+
+var image4 = document.getElementById ("rotate4");
+image4.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image4.addEventListener("mouseout", function () {
+	var image4 = this;
+	image4.className = "out2";
+	window.setTimeout (function () { image4.className = "" }, 90 );
+}, false );
+
+var image5 = document.getElementById ("rotate5");
+image5.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image5.addEventListener("mouseout", function () {
+	var image5 = this;
+	image5.className = "out1";
+	window.setTimeout (function () { image5.className = "" }, 90 );
+}, false );
+
+var image6 = document.getElementById ("rotate6");
+image6.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image6.addEventListener("mouseout", function () {
+	var image6 = this;
+	image6.className = "out";
+	window.setTimeout (function () { image6.className = "" }, 90 );
+}, false );
+
+var image7 = document.getElementById ("rotate7");
+image7.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image7.addEventListener("mouseout", function () {
+	var image7 = this;
+	image7.className = "out1";
+	window.setTimeout (function () { image7.className = "" }, 90 );
+}, false );
+
+var image8 = document.getElementById ("rotate8");
+image8.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image8.addEventListener("mouseout", function () {
+	var image8 = this;
+	image8.className = "out2";
+	window.setTimeout (function () { image8.className = "" }, 90 );
+}, false );
+
+var image9 = document.getElementById ("rotate9");
+image9.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image9.addEventListener("mouseout", function () {
+	var image9 = this;
+	image9.className = "out";
+	window.setTimeout (function () { image9.className = "" }, 90 );
+}, false );
+
+var image10 = document.getElementById ("rotate10");
+image10.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image10.addEventListener("mouseout", function () {
+	var image10 = this;
+	image10.className = "out2";
+	window.setTimeout (function () { image10.className = "" }, 90 );
+}, false );
+
+var image11 = document.getElementById ("rotate11");
+image11.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image11.addEventListener("mouseout", function () {
+	var image11 = this;
+	image11.className = "out";
+	window.setTimeout (function () { image11.className = "" }, 90 );
+}, false );
+
+var image12 = document.getElementById ("rotate12");
+image12.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image12.addEventListener("mouseout", function () {
+	var image12 = this;
+	image12.className = "out2";
+	window.setTimeout (function () { image12.className = "" }, 90 );
+}, false );
+
+var image13 = document.getElementById ("rotate13");
+image13.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image13.addEventListener("mouseout", function () {
+	var image13 = this;
+	image13.className = "out";
+	window.setTimeout (function () { image13.className = "" }, 90 );
+}, false );
+
+var image14 = document.getElementById ("rotate14");
+image14.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image14.addEventListener("mouseout", function () {
+	var image14 = this;
+	image14.className = "out2";
+	window.setTimeout (function () { image14.className = "" }, 90 );
+}, false );
+
+var image15 = document.getElementById ("rotate15");
+image15.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image15.addEventListener("mouseout", function () {
+	var image15 = this;
+	image15.className = "out2";
+	window.setTimeout (function () { image15.className = "" }, 90 );
+}, false );
+
+var image16 = document.getElementById ("rotate16");
+image16.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image16.addEventListener("mouseout", function () {
+	var image16 = this;
+	image16.className = "out1";
+	window.setTimeout (function () { image16.className = "" }, 90 );
+}, false );
+
+var image17 = document.getElementById ("rotate17");
+image17.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image17.addEventListener("mouseout", function () {
+	var image17 = this;
+	image17.className = "out";
+	window.setTimeout (function () { image17.className = "" }, 90 );
+}, false );
+
+var image18 = document.getElementById ("rotate18");
+image18.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image18.addEventListener("mouseout", function () {
+	var image18 = this;
+	image18.className = "out2";
+	window.setTimeout (function () { image18.className = "" }, 90 );
+}, false );
+
+var image19 = document.getElementById ("rotate19");
+image19.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image19.addEventListener("mouseout", function () {
+	var image19 = this;
+	image19.className = "out1";
+	window.setTimeout (function () { image19.className = "" }, 90 );
+}, false );
+
+var image20 = document.getElementById ("rotate20");
+image20.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image20.addEventListener("mouseout", function () {
+	var image20 = this;
+	image20.className = "out";
+	window.setTimeout (function () { image20.className = "" }, 90 );
+}, false );
+
+var image21 = document.getElementById ("rotate21");
+image21.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image21.addEventListener("mouseout", function () {
+	var image21 = this;
+	image21.className = "out";
+	window.setTimeout (function () { image21.className = "" }, 90 );
+}, false );
+
+var image22 = document.getElementById ("rotate22");
+image22.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image22.addEventListener("mouseout", function () {
+	var image22 = this;
+	image22.className = "out2";
+	window.setTimeout (function () { image22.className = "" }, 90 );
+}, false );
+
+var image23 = document.getElementById ("rotate23");
+image23.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image23.addEventListener("mouseout", function () {
+	var image23 = this;
+	image23.className = "out2";
+	window.setTimeout (function () { image23.className = "" }, 90 );
+}, false );
+
+var image24 = document.getElementById ("rotate24");
+image24.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image24.addEventListener("mouseout", function () {
+	var image24 = this;
+	image24.className = "out";
+	window.setTimeout (function () { image24.className = "" }, 90 );
+}, false );
+
+var image25 = document.getElementById ("rotate25");
+image25.addEventListener("mouseover", function () {
+	this.className = "over1";
+}, false );
+image25.addEventListener("mouseout", function () {
+	var image25 = this;
+	image25.className = "out1";
+	window.setTimeout (function () { image25.className = "" }, 90 );
+}, false );
+
+var image26 = document.getElementById ("rotate26");
+image26.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image26.addEventListener("mouseout", function () {
+	var image26 = this;
+	image26.className = "out2";
+	window.setTimeout (function () { image26.className = "" }, 90 );
+}, false );
+
+var image27 = document.getElementById ("rotate27");
+image27.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image27.addEventListener("mouseout", function () {
+	var image27 = this;
+	image27.className = "out2";
+	window.setTimeout (function () { image27.className = "" }, 90 );
+}, false );
+
+var image28 = document.getElementById ("rotate28");
+image28.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image28.addEventListener("mouseout", function () {
+	var image28 = this;
+	image28.className = "out2";
+	window.setTimeout (function () { image28.className = "" }, 90 );
+}, false );
+
+var image29 = document.getElementById ("rotate29");
+image29.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image29.addEventListener("mouseout", function () {
+	var image29 = this;
+	image29.className = "out";
+	window.setTimeout (function () { image29.className = "" }, 90 );
+}, false );
+
+var image30 = document.getElementById ("rotate30");
+image30.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image30.addEventListener("mouseout", function () {
+	var image30 = this;
+	image30.className = "out2";
+	window.setTimeout (function () { image30.className = "" }, 90 );
+}, false );
+
+var image31 = document.getElementById ("rotate31");
+image31.addEventListener("mouseover", function () {
+	this.className = "over";
+}, false );
+image31.addEventListener("mouseout", function () {
+	var image31 = this;
+	image31.className = "out";
+	window.setTimeout (function () { image31.className = "" }, 90 );
+}, false );
+
+var image32 = document.getElementById ("rotate32");
+image32.addEventListener("mouseover", function () {
+	this.className = "over2";
+}, false );
+image32.addEventListener("mouseout", function () {
+	var image32 = this;
+	image32.className = "out2";
+	window.setTimeout (function () { image32.className = "" }, 90 );
+}, false );
+
+
+
+});
